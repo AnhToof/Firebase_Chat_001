a610e65bb0bb14a56b04f7d86228a008f3ae7af5
Toof <letuananh.programming@gmail.com>
2018-11-14 15:53:55 +0700

    Authentication Screen - Design UI

---
+++ app/src/main/java/com/lobesoftware/toof/firebase_chat_001/screen/authentication/AuthenticationActivity.kt
@@ -0,0 +1,13 @@
+package com.lobesoftware.toof.firebase_chat_001.screen.authentication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.lobesoftware.toof.firebase_chat_001.R
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
---
+++ app/src/main/java/com/lobesoftware/toof/firebase_chat_001/screen/authentication/login/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.lobesoftware.toof.firebase_chat_001.screen.authentication.login
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lobesoftware.toof.firebase_chat_001.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
